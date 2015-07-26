190f75945e31a180ce59a9c348c7550e9e1b7a20
ZYROz <zyroz@users.noreply.github.com>
2015-07-26 17:32:00 +0530

    Add carousel & logos

--- src/js/zyro.js
+++ src/js/zyro.js
@@ -10,5 +10,34 @@ Zyro = function() {
 * @description Start executing functions
 */
 Zyro.prototype.initiate = function() {
-    console.log("Initiated!");
+    this.setupCarousel();
+};
+
+/**
+* @function Zyro.prototype.setupCarousel
+* @description Start executing functions
+*/
+
+Zyro.prototype.setupCarousel = function() {
+    $('#myCarousel').carousel({
+        interval: false
+    });
+
+    var clickEvent = false;
+    $('#myCarousel').on('click', '.nav a', function() {
+            clickEvent = true;
+            $('.nav li').removeClass('active');
+            $(this).parent().addClass('active');
+    }).on('slid.bs.carousel', function(e) {
+        if(!clickEvent) {
+            var count = $('.nav').children().length -1;
+            var current = $('.nav li.active');
+            current.removeClass('active').next().addClass('active');
+            var id = parseInt(current.data('slide-to'));
+            if(count == id) {
+                $('.nav li').first().addClass('active');
+            }
+        }
+        clickEvent = false;
+    });
 };
