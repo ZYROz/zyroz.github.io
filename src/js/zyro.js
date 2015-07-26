7530ed742dce3101726f424353db22a30fc5fd12
ZYROz <zyroz@users.noreply.github.com>
2015-07-26 13:41:07 +0530

    Structuring the page

---
+++ src/js/zyro.js
@@ -0,0 +1,14 @@
+/**
+* @class Zyro
+*/
+Zyro = function() {
+    this.initiate();
+};
+
+/**
+* @function Zyro.prototype.initiate
+* @description Start executing functions
+*/
+Zyro.prototype.initiate = function() {
+    console.log("Initiated!");
+};
