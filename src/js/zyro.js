bb2bef3c0e821c29356339cc22da120c3935913f
ZYROz <zyroz@users.noreply.github.com>
2015-11-01 22:47:55 +0530

    Adding Google Analytics

--- src/js/zyro.js
+++ src/js/zyro.js
@@ -11,6 +11,7 @@ Zyro = function() {
 */
 Zyro.prototype.initiate = function() {
     this.setupCarousel();
+    this.collectGoogleAnalytics();
 };
 
 /**
@@ -41,3 +42,13 @@ Zyro.prototype.setupCarousel = function() {
         clickEvent = false;
     });
 };
+
+Zyro.prototype.collectGoogleAnalytics = function() {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-69564612-1', 'auto');
+    ga('send', 'pageview');
+}
